744351133bbdd6d13612d165b5fa620ee5e05062
moskalevsa <moskalevsa@yandex.ru>
2026-02-16 12:16:28 +0300

    Управление БД

--- database.py
+++ database.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 
 #Настройка подключения к базе данных**
 DATABASE_URL = 'postgresql://postgres:Crba@localhost:5432/dronsevents'
-# Создание асинхронного двигателя базы данных
+# Создание двигателя базы данных
 engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 query = ("""
          SELECT eventsdatetime
@@ -16,6 +23,13 @@ query = ("""
            FROM events.dronevent;
            """)
 
-print (query)
+#print (query)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
